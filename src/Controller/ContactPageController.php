8e35b3fcb7fc6e104df4761fb0939cff0c7a9a2b
HugoAndre15 <hugo.andre1501@gmail.com>
2024-02-09 16:04:43 +0100

    Change route name

--- src/Controller/ContactPageController.php
+++ src/Controller/ContactPageController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class OnePageController extends AbstractController
+class ContactPageController extends AbstractController
 {
     /**
-     * @Route("/one-page", name="one_page")
+     * @Route("/contact", name="contact")
      */
     public function index(): Response
     {
-        return $this->render('one_page/index.html.twig');
+        return $this->render('_composants/contact.html.twig');
     }
 }
