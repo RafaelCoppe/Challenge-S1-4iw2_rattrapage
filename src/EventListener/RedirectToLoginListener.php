4806e563cc88ebf1c79073f130e2d30588dcf922
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-03-03 14:36:27 +0100

    add blocus hors login

---
+++ src/EventListener/RedirectToLoginListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+
+class RedirectToLoginListener
+{
+    private $router;
+    private $security;
+
+    public function __construct(RouterInterface $router, Security $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $route = $event->getRequest()->attributes->get('_route');
+
+        if (!$this->security->getUser() && $route !== 'app_login') {
+            $event->setResponse(new RedirectResponse($this->router->generate('app_login')));
+        }
+    }
+}
